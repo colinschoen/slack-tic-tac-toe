0760bdc6fc968a95e7aae2dbae3500d9c351bea4
Colin Schoen <cschoen@berkeley.edu>
2016-09-28 19:28:00 -0700

    Add endpoint
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 import utils
 
 SLACK_TOKEN = ""
+API_VERSION = "v1"
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,3 +11,5 @@ api = Api(app)
 class Board(Resource):
     def get(self):
         return utils.getBoard([[None, 0, 1], [None, 0, 0], [None, None]])
+
+api.add_resource(Board, '/{}/board'.format(API_VERSION))
