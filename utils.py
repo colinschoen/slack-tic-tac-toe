491de258fba055da199153948e7c01472d45ab75
Colin Schoen <cschoen@berkeley.edu>
2016-10-01 01:44:23 -0700

    Print nicely
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- utils.py
+++ utils.py
@@ -14,13 +14,13 @@ def getBoard(board):
     assert type(board) is list, "The board must be a list"
 
     return "| {} | {} | {} |\n|---+---+---|\n| {} | {} | {} |\n|---+---+---|\n| {} | {} | {} |".format(
-                board[0][0],
-                board[0][1],
-                board[0][2],
-                board[1][0],
-                board[1][1],
-                board[1][2],
-                board[2][0],
-                board[2][1],
-                board[2][2])
+                board[0][0] if board[0][0] else "  ",
+                board[0][1] if board[0][1] else "  ",
+                board[0][2] if board[0][2] else "  ",
+                board[1][0] if board[1][0] else "  ",
+                board[1][1] if board[1][1] else "  ",
+                board[1][2] if board[1][2] else "  ",
+                board[2][0] if board[2][0] else "  ",
+                board[2][1] if board[2][1] else "  ",
+                board[2][2] if board[2][2] else "  ")
 
