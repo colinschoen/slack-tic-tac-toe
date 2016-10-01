a4ae1cd01ebbe02d6da57c63c74b426f995e7541
Colin Schoen <cschoen@berkeley.edu>
2016-10-01 00:23:50 -0700

    Use config file
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, jsonify
 from flask_restful import Resource, Api
 import utils
 
-SLACK_TOKEN = ""
-API_VERSION = "v1"
+API_VERSION = 'v1'
 STARTING_BOARD = [[None, None, None], [None, None, None], [None, None, None]]
 
 app = Flask(__name__)
@@ -13,7 +12,10 @@ class Board(Resource):
     def get(self):
         return utils.getBoard([[None, 0, 1], [None, 0, 0], [None, None, 0]])
 
-api.add_resource(Board, '/{}/board'.format(API_VERSION))
+api.add_resource(Board, '/{}/board'.format(app.config['API_VERSION']))
 
 if __name__ == "__main__":
+    app.config.from_pyfile('config.cfg')
+    app.config['API_VERSION'] = API_VERSION
+    app.config['STARTING_BOARD'] = STARTING_BOARD
     app.run(host='0.0.0.0')
