41b340cf88684379975b9acf0b7a96ee124db176
Colin Schoen <cschoen@berkeley.edu>
2016-10-02 15:29:05 -0700

    Remove start_game logic from app.py
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- app.py
+++ app.py
@@ -36,20 +36,6 @@ class Hook(Resource):
                     'text': response
                })
 
-def start_game(channel, player0, player1):
-    """
-    Starts a new game in channel if one does not exist already
-    
-    Args:
-        channel(str): The Slack CID to start the game in
-        player0(str): The Slack UID of the first player in the game
-        player1(str): The Slack UID of the second player in the game
-        
-    Returns:
-        None
-    """
-    pass
-
 
 api.add_resource(Hook, '/{}/hook'.format(app.config['API_VERSION']))
 
