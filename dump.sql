4c176b7b24908f637010a85bcdbd87b877cde226
Colin Schoen <cschoen@berkeley.edu>
2016-10-02 19:41:38 -0700

    Add SQL dump
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

---
+++ dump.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE slack;
+CREATE TABLE games (id INT NOT NULL AUTO_INCREMENT,
+    player0_id VARCHAR(100) NOT NULL,
+    player1_id VARCHAR(100) NOT NULL,
+    player1_nickname VARCHAR(40) NOT NULL,
+    player_turn VARCHAR(100) NOT NULL,
+    channel_id VARCHAR(100) NOT NULL,
+    state VARCHAR(9) NOT NULL,
+    updated_at VARCHAR(100) NOT NULL,
+    created_at VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
