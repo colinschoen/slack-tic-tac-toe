31f4a2394a8cdc958ac5c1680c80d0d3d52a6d89
Colin Schoen <cschoen@berkeley.edu>
2016-10-06 14:06:16 -0700

    Update dump.sql
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- dump.sql
+++ dump.sql
@@ -2,6 +2,7 @@ CREATE DATABASE slack;
 CREATE TABLE boards(id INT NOT NULL AUTO_INCREMENT,
     player0_id VARCHAR(100) NOT NULL,
     player1_id VARCHAR(100) NOT NULL,
+    player0_nickname VARCHAR(40) NOT NULL,
     player1_nickname VARCHAR(40) NOT NULL,
     player_turn VARCHAR(100) NOT NULL,
     channel_id VARCHAR(100) NOT NULL,
